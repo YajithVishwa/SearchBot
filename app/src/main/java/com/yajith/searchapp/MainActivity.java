3dd75a0ae455327fdcfd9c6ecaa6a192b351d8ed
YajithVishwa <yajith@student.tce.edu>
2020-11-03 15:05:26 +0530

    final

--- app/src/main/java/com/yajith/searchapp/MainActivity.java
+++ app/src/main/java/com/yajith/searchapp/MainActivity.java
@@ -9,9 +9,11 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
@@ -22,6 +24,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -40,15 +43,15 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
     private ProgressDialog progressDialog;
     private EditText editText;
-    private String searchtext,responseMessage,result,finaltext="";
+    private String responseMessage,result,finaltext="";
     private Integer responseCode;
     private SpeechRecognizer speechRecognizer;
     private TextToSpeech textToSpeech;
     private TextView textView;
-    private MaterialButton button;
+    private ImageButton button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,39 +62,12 @@ public class MainActivity extends AppCompatActivity {
         speechRecognizer=SpeechRecognizer.createSpeechRecognizer(this);
         button=findViewById(R.id.search);
         ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.RECORD_AUDIO},200);
-        textToSpeech=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int i) {
-                if(i != TextToSpeech.ERROR) {
-                    textToSpeech.setLanguage(Locale.UK);
-                }
-            }
-        });
+        textToSpeech=new TextToSpeech(this,this);
         textView=findViewById(R.id.searchtext);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 editText.setText("");
-                /*searchtext=editText.getText().toString().trim().toLowerCase();
-              /*  if(searchtext.equals(""))
-                {
-                    Toast.makeText(MainActivity.this, "Search Text is Empty", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    Asyncs asyncs = new Asyncs();
-                    searchtext=searchtext.replaceAll(" ","+");
-                    String url="https://www.googleapis.com/customsearch/v1?q="+searchtext+"&key=AIzaSyDUkHycKC8v14QveA6pGasF9I82I6BCWRU"+"&cx=e768af4fbfa3e77d5"+"&alt=json";
-                    URL url1=null;
-                    try {
-                        url1=new URL(url);
-                    }
-                    catch (MalformedURLException e)
-                    {
-                        e.printStackTrace();
-                        Toast.makeText(MainActivity.this, "Error Occurred", Toast.LENGTH_SHORT).show();
-                    }
-                    asyncs.execute(url1);
-                }*/
                 Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
                 intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
                         RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
@@ -120,12 +96,17 @@ public class MainActivity extends AppCompatActivity {
                 if(a.contains("search"))
                 {
                     String[] b=a.split("search");
-                    editText.setText(b[1].trim());
+                    if(b.length==0)
+                    {
+                        textToSpeech.speak("Sorry I cant get it",TextToSpeech.QUEUE_FLUSH,null);
+                        return;
+                    }
+                    b[1]=b[1].trim();
+                    editText.setText(b[1]);
                     Asyncs asyncs = new Asyncs();
                     if(a.contains(" ")) {
                         a = b[1].replaceAll(" ", "+");
                     }
-                    Toast.makeText(this, a, Toast.LENGTH_SHORT).show();
                     String url="https://www.googleapis.com/customsearch/v1?q="+a+"&key=AIzaSyDUkHycKC8v14QveA6pGasF9I82I6BCWRU"+"&cx=e768af4fbfa3e77d5"+"&alt=json";
                     URL url1=null;
                     try {
@@ -139,8 +120,36 @@ public class MainActivity extends AppCompatActivity {
                     asyncs.execute(url1);
 
                 }
-                editText.setText(dat.get(0).toString());
+                else if(a.contains("open"))
+                {
+                    String[] b=a.split("open");
+                    if(b.length==0)
+                    {
+                        textToSpeech.speak("Sorry I cant get it",TextToSpeech.QUEUE_FLUSH,null);
+                        return;
+                    }
+                    b[1]=b[1].trim();
+                    Intent intent=new Intent(Intent.ACTION_WEB_SEARCH);
+                    intent.putExtra(SearchManager.QUERY,b[1].toString());
+                    startActivity(intent);
+                }
+                else
+                {
+                    textToSpeech.speak("Sorry I cant get it",TextToSpeech.QUEUE_FLUSH,null);
+                    return;
+                }
+            //    editText.setText(dat.get(0).toString());
             }
+        }
+    }
+
+    @Override
+    public void onInit(int i) {
+        if(i==TextToSpeech.SUCCESS)
+        {
+            textToSpeech.setLanguage(Locale.ENGLISH);
+            textToSpeech.setPitch(1);
+            textToSpeech.setSpeechRate(1);
         }
     }
 
@@ -210,13 +219,15 @@ public class MainActivity extends AppCompatActivity {
                 JSONObject jsonObject=new JSONObject(s);
                 JSONArray jsonArray=jsonObject.getJSONArray("items");
                 JSONObject jsonObject1=jsonArray.getJSONObject(0);
-                finaltext+="Title- "+jsonObject1.getString("title")+"\n";
+                finaltext="Title- "+jsonObject1.getString("title")+"\n";
                 finaltext+="Link- "+jsonObject1.getString("link")+"\n";
                 finaltext+="Description- "+jsonObject1.getString("snippet")+"\n";
                 progressDialog.dismiss();
                 textView.setText(finaltext);
             } catch (JSONException e) {
                 e.printStackTrace();
+                progressDialog.dismiss();
+                textToSpeech.speak("No results Found",TextToSpeech.QUEUE_FLUSH,null);
             }
         }
     }
